397c2927ad30879e431f7c199ab9781be4b979d4
arc <arc.onlineemail@gmail.com>
2025-10-12 23:39:46 +1100

    Crystal Shatter weapons, grease system stuff.

--- data/blueprints.append.lua
+++ data/blueprints.append.lua
@@ -67,13 +67,36 @@ systemsToAppend["aea_freeze"] = {attributes = {power = 1, start = "false"}, mann
         {room_image = "room_aea_freeze_6", w = 1, h = 2, top = "1", bottom = "1", left="00", right="00"},
         {room_image = "room_aea_freeze_7", w = 2, h = 1, top = "01", bottom = "01", left="0", right="0"},
         {room_image = "room_aea_freeze_8", w = 2, h = 1, top = "10", bottom = "01", left="1", right="1"},
-        {room_image = "room_aea_freeze_9", w = 2, h = 1, top = "00", bottom = "00", left="0", right="0"},
         {room_image = "room_aea_freeze_10", w = 2, h = 2, top = "10", bottom = "10", left="01", right="10"},
         {room_image = "room_aea_freeze_11", w = 2, h = 2, top = "00", bottom = "11", left="00", right="00"},
-        {room_image = "room_aea_freeze_12", w = 1, h = 2, top = "0", bottom = "0", left="10", right="10"}
+        {room_image = "room_aea_freeze_12", w = 1, h = 2, top = "0", bottom = "0", left="10", right="10"},
+        {room_image = "room_aea_freeze_9", w = 2, h = 1, top = "00", bottom = "00", left="0", right="0"}
     }
 }
-systemsToAppend["aea_grease"] = {attributes = {power = 1, start = "false"}, manning = false, replace_sys = "battery"}
+systemsToAppend["aea_grease"] = {attributes = {power = 1, start = "false"}, manning = false, replace_sys = "battery",
+    image_list = {
+        {room_image = "room_aea_grease_4", w = 2, h = 2, top = "10", bottom = "10", left="00", right="11"},
+        {room_image = "room_aea_grease", w = 2, h = 1, top = "11", bottom = "00", left="0", right="1"},
+        {room_image = "room_aea_grease_2", w = 2, h = 1, top = "00", bottom = "00", left="1", right="0"},
+        {room_image = "room_aea_grease_3", w = 2, h = 1, top = "11", bottom = "00", left="0", right="0"},
+        {room_image = "room_aea_grease_5", w = 1, h = 2, top = "1", bottom = "0", left="00", right="11"},
+        {room_image = "room_aea_grease_6", w = 1, h = 2, top = "0", bottom = "0", left="11", right="00"},
+        {room_image = "room_aea_grease_7", w = 2, h = 1, top = "00", bottom = "00", left="1", right="1"},
+        {room_image = "room_aea_grease_8", w = 1, h = 2, top = "0", bottom = "0", left="01", right="10"},
+        {room_image = "room_aea_grease_9", w = 2, h = 1, top = "00", bottom = "11", left="1", right="0"},
+        {room_image = "room_aea_grease_10", w = 2, h = 2, top = "0", bottom = "0", left="00", right="11"},
+        {room_image = "room_aea_grease_11", w = 1, h = 2, top = "0", bottom = "1", left="01", right="00"},
+        {room_image = "room_aea_grease_12", w = 2, h = 1, top = "01", bottom = "00", left="0", right="1"},
+        {room_image = "room_aea_grease_13", w = 2, h = 1, top = "01", bottom = "01", left="0", right="0"},
+        {room_image = "room_aea_grease_14", w = 2, h = 1, top = "00", bottom = "11", left="0", right="0"},
+        {room_image = "room_aea_grease_15", w = 1, h = 2, top = "0", bottom = "0", left="00", right="01"},
+        {room_image = "room_aea_grease_16", w = 1, h = 2, top = "0", bottom = "0", left="11", right="00"},
+        {room_image = "room_aea_grease_17", w = 1, h = 2, top = "0", bottom = "0", left="10", right="10"},
+        {room_image = "room_aea_grease_18", w = 1, h = 2, top = "0", bottom = "0", left="10", right="01"},
+        {room_image = "room_aea_grease_19", w = 2, h = 1, top = "10", bottom = "00", left="1", right="0"},
+        {room_image = "room_aea_grease_20", w = 1, h = 2, top = "1", bottom = "1", left="00", right="00"}
+    }
+}
 
 local function noDoorOverlap(rT, rB, rL, rR, iT, iB, iL, iR, shipName)
     local room = table.concat({rT,rB,rL,rR},"")
